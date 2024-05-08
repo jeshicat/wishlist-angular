77446a6411d7016855ea13248d772e98ebb93678
Jess K <business.grownup@gmail.com>
2024-05-08 14:44:46 -0400

    Figuring out ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wishlist';
-  items = ["apple", "pear", "banana"];
+  title = 'Shopping List';
+  items = [
+    {item: "apple", qty: 3, completed: false},
+    {item: "banana", qty: 5, completed: true},
+    {item: "pear", qty: 1, completed: false}
+  ];
 }
